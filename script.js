af1009375f254b7b0fb50394c9a3e24921ac373d
Thomas Eckersley <ecke2225@mylaurier.ca>
2023-09-16 16:37:06 -0400

    Improved playRound so console.log displays events

--- script.js
+++ script.js
@@ -11,27 +11,27 @@ function getComputerChoice(choice) {
 }
 
 function playerSelection() {
-    plChoice = prompt("Welcome to Rock, Paper, Scissors! Make your choice below:")
+    plChoice = prompt("Welcome to Rock, Paper, Scissors! Make Your Choice Below:")
     plChoice = plChoice.charAt(0).toUpperCase() + plChoice.slice(1)
     return "You Chose: " + plChoice
 }
 
 function playRound(playerSelection, getComputerChoice) {
-    playerSelection()
-    getComputerChoice()
+    console.log(playerSelection())
+    console.log(getComputerChoice())
     if (computerChooses===plChoice) {
         console.log("It's a Draw!");
     } else if (computerChooses==="Rock"&&plChoice==="Paper") {
         console.log("You Win!");
     } else if (computerChooses==="Rock"&&plChoice==="Scissors") {
-        console.log("You Lose!");
+        console.log("You Lose! Rock Beats Scissors!");
     } else if (computerChooses==="Paper"&&plChoice==="Scissors") {
         console.log("You Win!");
     } else if (computerChooses==="Paper"&&plChoice==="Rock") {
-        console.log("You Lose!");
+        console.log("You Lose! Paper Beats Rock!");
     } else if (computerChooses==="Scissors"&&plChoice==="Rock") {
         console.log("You Win!");
     } else if (computerChooses==="Scissors"&&plChoice==="Paper") {
-        console.log("You Lose!");
+        console.log("You Lose! Scissors Beats Paper!");
     }
 }
