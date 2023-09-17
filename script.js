0150d52b9b05acbfb1f64534c2b23bae43dd0035
Thomas Eckersley <ecke2225@mylaurier.ca>
2023-09-16 21:42:18 -0400

    Began counter and showing game score

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let playerScore = 0;
+let computerScore = 0;
+let games = 0;
+
 function getComputerChoice() {
     let choice = Math.floor((Math.random()*3)+1)
     if (choice===1) {
@@ -20,22 +24,63 @@ function playRound(playerSelection, getComputerChoice) {
     console.log(playerSelection())
     console.log(getComputerChoice())
     if (computerChooses===plChoice) {
+        games++;
         console.log("It's a Draw!");
     } else if (computerChooses==="Rock"&&plChoice==="Paper") {
+        playerScore++;
+        games++;
         console.log("You Win!");
     } else if (computerChooses==="Rock"&&plChoice==="Scissors") {
+        computerScore++;
+        games++;
         console.log("You Lose! Rock Beats Scissors!");
     } else if (computerChooses==="Paper"&&plChoice==="Scissors") {
+        playerScore++;
+        games++;
         console.log("You Win!");
     } else if (computerChooses==="Paper"&&plChoice==="Rock") {
+        computerScore++;
+        games++;
         console.log("You Lose! Paper Beats Rock!");
     } else if (computerChooses==="Scissors"&&plChoice==="Rock") {
+        playerScore++;
+        games++
         console.log("You Win!");
     } else if (computerChooses==="Scissors"&&plChoice==="Paper") {
+        computerScore++;
+        games++;
         console.log("You Lose! Scissors Beats Paper!");
+    } else if (playerScore=5) {
+        console.log("You Win! Ultimate Victory!");
+        let playerScore = playerScore - playerScore;
+        let computerScore = computerScore - computerScore;
+        let games = games - games;
+    } else if (computerScore=5) {
+        console.log("You Lose! Ultimate Defeat!");
+        let playerScore = playerScore - playerScore;
+        let computerScore = computerScore - computerScore;
+        let games = games - games;
     }
 }
 
 function game(playRound) {
     playRound(playerSelection,getComputerChoice)
+    gameScore = "After " + games + " games, the score is You: " + playerScore + " - Computer: " + computerScore;
+    console.log(gameScore);
+    playRound(playerSelection,getComputerChoice)
+    console.log(gameScore);
+    playRound(playerSelection,getComputerChoice)
+    console.log(gameScore);
+    playRound(playerSelection,getComputerChoice)
+    console.log(gameScore);
+    playRound(playerSelection,getComputerChoice)
+    console.log(gameScore);
+    playRound(playerSelection,getComputerChoice)
+    console.log(gameScore);
+    playRound(playerSelection,getComputerChoice)
+    console.log(gameScore);
+    playRound(playerSelection,getComputerChoice)
+    console.log(gameScore);
+    playRound(playerSelection,getComputerChoice)
+    console.log(gameScore);
 }
